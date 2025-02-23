842d012b19f188c999439b947f8c59ae32b19185
akram <medanimohamedakram@gmail.com>
2025-02-23 14:45:14 +0100

    Some polishing

--- touchpadToggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-state="$(gsettings get org.gnome.desktop.peripherals.touchpad send-events)"
-if [[ "$state" == "'disabled'" ]]; then
-  gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
-else 
-  gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled
-fi
