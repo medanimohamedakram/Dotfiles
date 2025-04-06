446d0007b0c2d6ac209ce0d91a1d9ddb939839b5
Aakrima <medanimohamedakram@proton.me>
2025-04-06 20:28:52 +0100

    switching to foot, waybar and dunst\narchiving some files and doing some cleanup

--- eww/scripts/volume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-
-wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o MUTED || wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o "[+-]?([0-9]*[.])?[0-9]+" | awk '{ print $0 * 100 }'
-
-pactl subscribe | rg --line-buffered "on sink" | while read -r _; do
-wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o MUTED || wpctl get-volume @DEFAULT_AUDIO_SINK@ | rg -o "[+-]?([0-9]*[.])?[0-9]+" | awk '{ print $0 * 100 }'
-done
